366406e2036ba023dc4135e843eacb1164e93ea8
CapitanJek <azim.esenov.0@example.com>
2022-04-11 20:02:27 +0600

    frist

---
+++ fizzBuzz.js
@@ -0,0 +1,16 @@
+let num = 1;
+
+for (; num < 100; num++) {
+
+    if (num % 5 === 0 && num % 3 === 0) {
+
+        console.log(num, " FizzBuzz ")
+
+    } else if (num % 5 === 0) {
+        console.log(num, " Buzz ");
+    } else if (num % 3 === 0) {
+        console.log(num," Fizz ");
+    } else {
+        console.log(num);
+    }
+}
---
+++ main.js
@@ -0,0 +1,6 @@
+let stars = "*";
+
+for(let i = 0; i<7; i++){
+    console.log(stars);
+    stars+="*";
+}
